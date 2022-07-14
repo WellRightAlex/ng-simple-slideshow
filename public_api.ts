7c3bad14f0620d7e3b607d009216314be5086f0e
Alex McArdle <amcardle@wellright.com>
2022-07-14 13:46:01 -0500

    update package configuration

--- public_api.ts
+++ public_api.ts
@@ -1,2 +1,3 @@
 export * from './src/app/modules/slideshow/slideshow.module';
 export {IImage} from './src/app/modules/slideshow/IImage';
+export * from './src/app/modules/slideshow/slideshow.component';
